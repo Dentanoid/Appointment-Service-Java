787f44ef6cfb6a0d4e4c052aecafa6303a580aa5
joelmat <joel.mattsson@hotmail.se>
2023-11-17 15:03:28 +0100

    #1 (on-going) Add initial structure

---
+++ AppointmentService/src/main/java/org/example4/AppointmentService.java
@@ -0,0 +1,13 @@
+package org.example4;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AppointmentService {
+    public static void main(String[] args) {
+        MQTTHandler mqttHandler = new MQTTHandler();
+    }
+}
