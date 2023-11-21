f6ea7a17e0f5d74e149c3c68a2abf08d949d4757
joelmat <joelmat@student.chalmers.se>
2023-11-21 04:19:30 +0100

    #8 (on-going) Make 'create available times' for dentists executable on the new refactored code structure

--- AppointmentService/src/main/java/org/example4/TopicManagement/Dentist.java
+++ AppointmentService/src/main/java/org/example4/TopicManagement/Dentist.java
@@ -2,20 +2,22 @@ package org.example4.TopicManagement;
 
 import org.bson.Document;
 import org.example4.DatabaseManager;
+import org.example4.MqttManager;
 import org.example4.Schemas.AvailableTimes;
 
 public class Dentist implements Client {
     private String topic;
+    private Document payloadDoc;
 
-    public Dentist(String topic) {
+    public Dentist(String topic, String payload) {
         this.topic = topic;
-        executeRequestedOperation(topic);
+        executeRequestedOperation(topic, payload);
     }
 
     @Override
-    public void createAppointment() {
-        // Document availableTimesDocument = DatabaseManager.convertPayloadToDocument(payload, new AvailableTimes());        
-        // DatabaseManager.saveDocumentInCollection(DatabaseManager.availableTimesCollection, availableTimesDocument);        
+    public void createAppointment(String payload) {
+        payloadDoc = DatabaseManager.convertPayloadToDocument(payload, new AvailableTimes());        
+        DatabaseManager.saveDocumentInCollection(DatabaseManager.availableTimesCollection, payloadDoc);        
     }
 
     @Override
@@ -24,20 +26,21 @@ public class Dentist implements Client {
     }
 
     @Override
-    public void executeRequestedOperation(String topic) {
+    public void executeRequestedOperation(String topic, String payload) {
         String operation = decodeRequestedOperation(topic);
 
-        if (operation == "create") {
-            createAppointment();
+        if (operation.equals("create")) {
+            createAppointment(payload);
         } else {
             deleteAppointment();
         }
 
-        // mqttMain.publishMessage(topic, availableTimesDocument.toJson());
+        MqttManager.getMqttManager().publishMessage("pub/dentist/availabletime/create", payloadDoc.toJson());
     }
 
     // NOTE: Elaborate on this method once group has decided on structure of topic
     private String decodeRequestedOperation(String topic) {
-        return topic;
+        String[] splitString = topic.split("/"); // "sub/dentist/availabletime/create" 
+        return splitString[splitString.length - 1];
     }
 }
