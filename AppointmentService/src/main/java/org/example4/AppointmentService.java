c1f7e806d54335311dcdbcdde8a349bb41681d2b
joelmat <joel.mattsson@hotmail.se>
2023-11-22 15:57:35 +0100

    #8 (completed) Finalize refactoring of new code structure and integration of compliance with single principle responsibility

--- AppointmentService/src/main/java/org/example4/AppointmentService.java
+++ AppointmentService/src/main/java/org/example4/AppointmentService.java
@@ -29,47 +29,8 @@ public class AppointmentService {
         System.out.println("MANAGE RECIEVED PAYLOAD");
         System.out.println("**********************************************");
 
-        TopicManager topicManager = new TopicManager(topic, payload);
+        // TopicManager topicManager = new TopicManager(topic, payload);
+        TopicManager topicManager = new TopicManager();
+        topicManager.manageTopic(topic, payload);
     }
-
-    /*
-    private static void patientDeleteAppointment(String payload) {
-        JsonNode jsonNode = Utils.deserialize(payload);
-        String objectId = jsonNode.get("_id").toString();
-
-        ObjectId appointmentId;
-
-        try {
-            appointmentId = new ObjectId(objectId);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid ObjectId format");
-            return;
-        }
-
-        Bson searchQuery = new Document("_id", appointmentId);
-
-        try {
-            Document foundDocument = DatabaseManager.appointmentsCollection.find(searchQuery).first();
-
-            if (foundDocument != null) {
-                // Delete from the Appointments collection and get the document
-                Document deletedDocument = DatabaseManager.appointmentsCollection.findOneAndDelete(searchQuery);
-                // MqttManager.getMqttManager().publishMessage("grp20/notification/patient/cancel", deletedDocument.toJson());
-                mqttManager.publishMessage("grp20/notification/patient/cancel", deletedDocument.toJson());
-
-                // Remove the "patient_id" field from the document
-                deletedDocument.remove("patient_id");
-
-                // Insert the modified document into the AvailableTimes collection
-                DatabaseManager.availableTimesCollection.insertOne(deletedDocument);
-
-                System.out.println("Document deleted, patient_id removed, and migrated successfully.");
-            } else {
-                System.out.println("Object with this objectId is not found");
-            }
-        } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        }
-    }
-    */
 }
